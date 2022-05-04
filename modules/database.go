d910aaf354354e6d7064bb7889e84f9345c1e72a
new <chayanin.pia@dohome.co.th>
2022-05-04 13:54:49 +0700

    x

--- modules/database.go
+++ modules/database.go
@@ -8,7 +8,7 @@ import (
 
 func DbConnect() (*xorm.Engine, error) {
 
-	dataSoruce := `host='localhost' port=5432 user='chayanin' password='anymind' dbname='bitcoin' sslmode=false`
+	dataSoruce := `host='localhost' port=9998 user='chayanin' password='anymind' dbname='bitcoin' sslmode=false`
 	dx, err := xorm.NewEngine(`postgress`, dataSoruce)
 	if err != nil {
 		return nil, fmt.Errorf(err.Error())
