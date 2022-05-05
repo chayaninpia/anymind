5b2d5f8549526d369ecf80fe6700f227eb278604
new <chayanin.pia@dohome.co.th>
2022-05-05 14:58:42 +0700

    x

--- modules/bitcoin-create.go
+++ modules/bitcoin-create.go
@@ -17,10 +17,12 @@ func BitcoinCreate(c *gin.Context) {
 
 	req := BitcoinWallet{}
 	if err := c.ShouldBindJSON(&req); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
 		log.Panicln(err.Error())
 	}
 
 	if req.Amount == nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, `amount should be not null`)
 		log.Panicln(`amount should be not null`)
 	}
 	if req.DateTime == nil {
@@ -30,10 +32,12 @@ func BitcoinCreate(c *gin.Context) {
 
 	dx, err := DbConnect()
 	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
 		log.Panicln(err.Error())
 	}
 
 	if _, err := dx.Table(`bitcoin_wallet`).InsertOne(&req); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
 		log.Panicln(err.Error())
 	}
 
